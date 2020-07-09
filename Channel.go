e7794d4d33d3010dc7aebba80b0c3f154d214986
Andrew Horn <arhorn@smcm.edu>
2020-07-09 13:00:40 -0500

    initial commit

---
+++ Channel.go
@@ -0,0 +1,32 @@
+package ch
+
+import "../msg"
+
+// Channels is a slice of all Channels
+var Channels []*Channel
+
+// Channel is a channel
+type Channel struct {
+	Name     string        `json:"name"`
+	Messages []msg.Message `json:"messages"`
+}
+
+// CreateChannel function
+//	- create channel with name, empty slice
+//	- check if channel is unique
+//		- if not return error
+//	- add to the Channels slice
+
+// Jasmine --
+// AddMessage function
+//	- add sent message to Messages slice
+
+// GetMessages function
+//	- takes in channel name
+//	- returns slice of all messages in that channel
+
+// clearChannels function
+//	- sets Channels to empty slice
+
+// GetChannelNames function
+//	- returns slice of all channel names
